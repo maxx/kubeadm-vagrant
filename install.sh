30554eef7fa061077d29da871fa98a6472438281
James Vautin <james@vautin.com>
2017-03-29 11:04:22 -0400

    Lock install to kube 1.5.6

--- install.sh
+++ install.sh
@@ -7,4 +7,6 @@ cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
-apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
+apt-get install -y docker.io kubelet=1.5.6-00 kubectl=1.5.6-00 kubernetes-cni=0.5.1-00
+apt-get download kubectl=1.5.6-00
+dpkg --force-all -i kubeadm_1.5.6-00_amd64.deb
